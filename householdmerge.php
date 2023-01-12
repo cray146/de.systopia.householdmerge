2dbd0ea523fc5dd0786e2103aa76d223ce30ce8b
Chris Raymaekers <cray146@gmail.com>
2023-01-12 15:53:26 +0100

    Upgrade civix boilerplate code using civix upgrade command.

--- householdmerge.php
+++ householdmerge.php
@@ -56,17 +56,6 @@ function householdmerge_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param $files array(string)
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function householdmerge_civicrm_xmlMenu(&$files) {
-  _householdmerge_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -76,94 +65,12 @@ function householdmerge_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function householdmerge_civicrm_uninstall() {
-  _householdmerge_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function householdmerge_civicrm_enable() {
   _householdmerge_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function householdmerge_civicrm_disable() {
-  _householdmerge_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function householdmerge_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _householdmerge_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function householdmerge_civicrm_managed(&$entities) {
-  _householdmerge_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function householdmerge_civicrm_caseTypes(&$caseTypes) {
-  _householdmerge_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function householdmerge_civicrm_angularModules(&$angularModules) {
-  _householdmerge_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function householdmerge_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _householdmerge_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -182,4 +89,13 @@ function householdmerge_civicrm_merge($type, &$data, $mainId = NULL, $otherId = 
  */
 function householdmerge_civicrm_alterAPIPermissions($entity, $action, &$params, &$permissions) {
   $permissions['contact']['create_household'] = CRM_Householdmerge_Logic_Configuration::getCreateHouseholdPermission();
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function householdmerge_civicrm_entityTypes(&$entityTypes) {
+  _householdmerge_civix_civicrm_entityTypes($entityTypes);
 }
